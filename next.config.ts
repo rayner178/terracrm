4c1653a62ef0bf8361ef89ee4f3e0d772a05d4bb
Rayner Rodriguez <rodriguezray458@gmail.com>
2026-04-26 11:20:53 -0400

    fix: explicit next-intl plugin path

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 import withPWAInit from '@ducanh2912/next-pwa';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {};
 
